715e0970deb7b0de8054f578944bc89073d95924
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-09 14:06:24 +0100

    configuration frontend folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
 import "./App.scss";
+import Button from "@/components/ui/Button";
+import Card from "@admin/components/ui/Card";
+import { formatDate } from "@shared/utils/formatDate";
 
 export default function App() {
-  return <div>App</div>;
+  const now = new Date();
+  console.log(formatDate(now, "fr-FR"));
+
+  return (
+    <div>
+      <h1>App</h1>
+      <Button />
+      <Card />
+    </div>
+  );
 }
