6a160ffce28b1d745c019ebd7faa1619677fbe8f
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-10 14:03:52 +0100

    feat(app): setup routing and token management

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,12 +1,60 @@
 import "./App.scss";
-import { formatDate } from "@shared/utils/formatDate";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Add from "./pages/AddProduct/AddProduct";
+import ListProduct from "./pages/ListProduct/ListProduct";
+import Orders from "./pages/Orders/Orders";
+import { useEffect, useState } from "react";
+import Login from "./components/Login/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export default function App() {
-  console.log(formatDate(new Date(), "fr-FR"));
+  const [token, setToken] = useState<string>(
+    localStorage.getItem("token") || ""
+  );
+  const location = useLocation();
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+
   return (
     <div>
-      <h1>Admin</h1>
-      <p>Admin page</p>
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <div className="app-container">
+            <Sidebar />
+            <div className="content">
+              <h1 className="admin-title">ADMIN</h1>
+              {location.pathname === "/" && (
+                <p className="admin-description">
+                  Bienvenue sur la page d'administration de l'application de
+                  gestion de produits.
+                  <br />
+                  Choisissez une action dans le menu.
+                </p>
+              )}
+              <Routes>
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<ListProduct token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
