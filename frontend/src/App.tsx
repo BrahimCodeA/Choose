3c51b16cedfb7bb5d081a25469c8929a54a8b421
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-10 14:30:26 +0100

    Setup routing and layout structure in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,31 @@
 import "./App.scss";
-import Button from "@/components/ui/Button";
-import Card from "@admin/components/ui/Card";
-import { formatDate } from "@shared/utils/formatDate";
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Cart from "./pages/Cart/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile/Profile";
+import Men from "./pages/Men/Men";
+import Women from "./pages/Women/Women";
+import Kid from "./pages/Kid/Kid";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
 
-export default function App() {
-  const now = new Date();
-  console.log(formatDate(now, "fr-FR"));
-
+function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <Button />
-      <Card />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/kid" element={<Kid />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+      <ScrollUp />
+    </BrowserRouter>
   );
 }
+
+export default App;
