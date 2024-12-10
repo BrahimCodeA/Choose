275318f59810099d1d152f82f6469cd66c2f37a0
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-10 14:35:09 +0100

    Implement responsive navbar carousel with slick slider

---
+++ frontend/src/components/CarouselNav2/CarouselNav2.tsx
@@ -0,0 +1,21 @@
+import "./CarouselNav2.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { sliderSettings } from "../../config/sliderSettings";
+import { icons, titles } from "../../constants/carouselNav2Data";
+
+export default function NavbarCarousel() {
+  return (
+    <nav className="navbar-2">
+      <Slider {...sliderSettings}>
+        {icons.map((icon, index) => (
+          <div key={index} className="navbar2-items">
+            <span>{icon}</span>
+            <h4>{titles[index % titles.length]}</h4>
+          </div>
+        ))}
+      </Slider>
+    </nav>
+  );
+}
