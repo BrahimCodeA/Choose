605a22c4e2c6b5dee642ea2df09aa59fef871977
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-10 14:05:06 +0100

    Integrate Redux store with Provider

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
 import App from "./App.tsx";
+import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store.ts";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
