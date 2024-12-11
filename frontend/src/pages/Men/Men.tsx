d2edfed2020be3b551877c5874d2a4a1daf817c5
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-12-11 09:05:06 +0100

    create page category component

---
+++ frontend/src/pages/Men/Men.tsx
@@ -0,0 +1,5 @@
+import "./Men.scss";
+
+export default function Men() {
+  return <div>Men</div>;
+}
---
+++ frontend/src/pages/Women/Women.tsx
@@ -0,0 +1,5 @@
+import "./Women.scss";
+
+export default function Women() {
+  return <div>Women</div>;
+}
